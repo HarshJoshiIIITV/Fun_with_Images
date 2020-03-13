514a5c519473d6dceb0d2429f99e0afa89563966
Harsh Joshi <joshih1998@gmail.com>
2020-03-13 11:15:18 +0530

    intial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter,Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Images from '../containers/Images';
 import Image from '../containers/Image';
 
@@ -8,14 +8,13 @@ import Image from '../containers/Image';
 {/*className="bb bw1 pb3" are used for styling using Tachyons*/}
 const App=()=>{
     return(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <div className="tc">
             <h1 className="bb bw1 pb3">Fun With Images </h1>    
+             {/*Image component loads you the details of Image you click*/}
             <Route exact path='/' component={Images}/>   {/*Images component loads you the list of Images for both paths*/}
-            <Route exact path="/images" component={Images}/>
-            <Route path="/images/:image_id" component={Image}/> {/*Image component loads you the details of Image you click*/}
+            <Route exact path='/images' component={Images}/>
+            <Route path="/images/:image_id" component={Image}/>
           </div>
-        </BrowserRouter>
       )
 }
 
